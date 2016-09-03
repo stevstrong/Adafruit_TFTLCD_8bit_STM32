36821d94e8793f5e6bbc9b2ab6105d42ce52cdd0
stevstrong <stevestrong@online.de>
2016-09-03 16:04:32 +0200

    removed pin definitions, done in the header file

--- examples/rotationtest/rotationtest.ino
+++ examples/rotationtest/rotationtest.ino
@@ -5,28 +5,7 @@
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD_8bit_STM32.h> // Hardware-specific library
 
-// The control pins for the LCD can be assigned to any digital or
-// analog pins...but we'll use the analog pins as this allows us to
-// double up the pins with the touch screen (see the TFT paint example).
-#define LCD_CS PA3 // Chip Select goes to Analog 3
-#define LCD_CD PA2 // Command/Data goes to Analog 2
-#define LCD_WR PA1 // LCD Write goes to Analog 1
-#define LCD_RD PA0 // LCD Read goes to Analog 0
-
-#define LCD_RESET PB8 // Can alternately just connect to Arduino's reset pin
-
-// When using the BREAKOUT BOARD only, use these 8 data lines to the LCD:
-// For the Arduino Uno, Duemilanove, Diecimila, etc.:
-//   D0 connects to digital pin 8  (Notice these are
-//   D1 connects to digital pin 9   NOT in order!)
-//   D2 connects to digital pin 2
-//   D3 connects to digital pin 3
-//   D4 connects to digital pin 4
-//   D5 connects to digital pin 5
-//   D6 connects to digital pin 6
-//   D7 connects to digital pin 7
-// For the Arduino Mega, use digital pins 22 through 29
-// (on the 2-row header at the end of the board).
+// check the pin connections in the header file.
 
 // Assign human-readable names to some common 16-bit color values:
 #define	BLACK   0x0000
@@ -38,10 +17,9 @@
 #define YELLOW  0xFFE0
 #define WHITE   0xFFFF
 
-// Adafruit_TFTLCD tft(LCD_CS, LCD_CD, LCD_WR, LCD_RD, LCD_RESET);
-// If using the shield, all control and data lines are fixed, and
-// a simpler declaration can optionally be used:
+
 Adafruit_TFTLCD_8bit_STM32 tft;
+
 
 void setup(void) {
   Serial.begin(9600);
