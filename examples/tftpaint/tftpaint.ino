3f3a5d8d89a6467fb27d95c3458adfca7d4a79b2
stevstrong <stevestrong@online.de>
2016-09-04 17:37:46 +0200

    added repeated touch and double click features

--- examples/tftpaint/tftpaint.ino
+++ examples/tftpaint/tftpaint.ino
@@ -4,7 +4,7 @@
 
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_TFTLCD_8bit_STM32.h> // Hardware-specific library
-#include <TouchScreen_STM.h>
+#include <TouchScreen_STM32.h>
 
 
 // Assign human-readable names to some common 16-bit color values:
@@ -21,9 +21,22 @@
 
 Adafruit_TFTLCD_8bit_STM32 tft;
 
-#define BOXSIZE 40
-#define PENRADIUS 3
+#define BOX_X 30
+#define BOX_Y 40
+//#define PENRADIUS 3
+int penradius;
+// test colours
+#define X_WHITE		0
+#define X_YELLOW	(BOX_X)
+#define X_CYAN		(BOX_X*2)
+#define X_GREEN		(BOX_X*3)
+#define X_MAGENTA	(BOX_X*4)
+#define X_RED		(BOX_X*5)
+#define X_BLUE		(BOX_X*6)
+#define X_BLACK		(BOX_X*7)
+
 int oldcolor, currentcolor;
+int old_x, current_x;
 /*
 #define TFT_RD         PA0
 #define TFT_WR         PA1
@@ -43,10 +56,11 @@ char str[250];	// used for sprintf
 void setup(void)
 {
 	Serial.begin(115200);
+	
 	delay(6000); // allow time for OS to enumerate USB as COM port
+	
 	Serial.print("\n*** Paint demo with easy touch calibration process ***\n");
 
-	//Serial.println("Resetting the screen...");
 	uint16_t identifier = 0;
 do {
 	tft.reset();
@@ -64,22 +78,18 @@ do {
   } else {
     Serial.print(F("Unknown LCD driver chip: "));
     Serial.println(identifier, HEX);
- /*   Serial.println(F("If using the Adafruit 2.8\" TFT Arduino shield, the line:"));
-    Serial.println(F("  #define USE_ADAFRUIT_SHIELD_PINOUT"));
-    Serial.println(F("should appear in the library header (Adafruit_TFT.h)."));
-    Serial.println(F("If using the breakout board, it should NOT be #defined!"));
-    Serial.println(F("Also if using the breakout, double-check that all wiring"));
-    Serial.println(F("matches the tutorial."));
-*/
     identifier = 0;
   }
-  //delay(250);
+  delay(200);
 }  while (identifier == 0);
 
   tft.begin(identifier);
-  
-	ts.rangeSet(TFTWIDTH, TFTHEIGHT);	// set display X and Y resolution of the display screen
-							// the returned coordinates will be automatically mapped to these ranges
+	// Touch screen range
+	// set display X and Y range of the display screen
+	// the returned coordinates will be automatically mapped to these values.
+	// If this is not used, the default values form the header file are used.
+	ts.rangeSet(TFTWIDTH, TFTHEIGHT);	
+							
 
 // Calibration
 // needs sequentially touching two, diagonally opposite corners (check serial output!)
@@ -96,86 +106,98 @@ do {
   tft.println("Now press the opposite corner...");
 	Serial.print("ok.\n> and now press the diagonally opposite corner...");
 	ts.calibratePoint();
+  tft.setTextSize(2);
+  tft.setCursor(0, 200);
   tft.println("Calibration done.");
 	Serial.println("ok.\nCalibration done.");
-  delay(1000);
-  tft.fillScreen(BLACK);
-  //tft.setRotation(2);
 
-  tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
-  tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
-  tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, GREEN);
-  tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, CYAN);
-  tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, BLUE);
-  tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, MAGENTA);
-  // tft.fillRect(BOXSIZE*6, 0, BOXSIZE, BOXSIZE, WHITE);
- 
-  tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
-  currentcolor = RED;
+	delay(1000);
+
+  tft.fillScreen(BLACK);
+	// display test color boxes
+	tft.fillRect(X_WHITE, 0, BOX_X, BOX_Y, WHITE);
+	tft.fillRect(X_YELLOW, 0, BOX_X, BOX_Y, YELLOW);
+	tft.fillRect(X_CYAN, 0, BOX_X, BOX_Y, CYAN);
+	tft.fillRect(X_GREEN, 0, BOX_X, BOX_Y, GREEN);
+	tft.fillRect(X_MAGENTA, 0, BOX_X, BOX_Y, MAGENTA);
+	tft.fillRect(X_RED, 0, BOX_X, BOX_Y, RED);
+	tft.fillRect(X_BLUE, 0, BOX_X, BOX_Y, BLUE);
+	tft.fillRect(X_BLACK, 0, BOX_X, BOX_Y, BLACK);
+  
+  currentcolor = oldcolor = WHITE;
+  current_x = old_x = X_WHITE;
+  tft.drawRect(X_WHITE, 0, BOX_X, BOX_Y, BLACK); // box selection
+  penradius = 3;
  
   pinMode(LED_PIN, OUTPUT);
 }
 
-#define MINPRESSURE 10
-#define MAXPRESSURE 1000
+#define PAINT_MARGIN 10
 
 void loop()
 {
 	TSPoint p;	// a point object holds x, y and z coordinates
 /*
-  digitalWrite(LED_PIN, HIGH);
   TSPoint p = ts.getPoint();
-  digitalWrite(LED_PIN, LOW);
 */
 
   // we have some minimum pressure we consider 'valid'
   // pressure of 0 means no pressing!
   
-  if ( ts.getPoint(&p) ) { //  returns 'true' if touch is detected, otherwise 'false'
+	if ( ts.getPoint(&p) ) { //  returns 'true' if touch is detected, otherwise 'false'
+		digitalWrite(LED_PIN, HIGH);
 		// the coordinates will be mapped to the set display ranges by using 'rangeSet()' in setup
 		// x, y (in pixels) can be directly used for display routines!
 		//sprintf(str,"X = %4i, Y = %4i, P = %4i\n", p.x, p.y, p.z);	Serial.print(str);
-    /*
-    if (p.y < (TS_MINY-5)) {
-      Serial.println("erase");
-      // press the bottom of the screen to erase 
-      tft.fillRect(0, BOXSIZE, tft.width(), tft.height()-BOXSIZE, BLACK);
-    }*/
-    if (p.y < BOXSIZE) {
-       oldcolor = currentcolor;
 
-       if (p.x < BOXSIZE) { 
-         currentcolor = RED; 
-         tft.drawRect(0, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*2) {
-         currentcolor = YELLOW;
-         tft.drawRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*3) {
-         currentcolor = GREEN;
-         tft.drawRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*4) {
-         currentcolor = CYAN;
-         tft.drawRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*5) {
-         currentcolor = BLUE;
-         tft.drawRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, WHITE);
-       } else if (p.x < BOXSIZE*6) {
-         currentcolor = MAGENTA;
-         tft.drawRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, WHITE);
-       }
+		if (p.y < BOX_Y) {
+			// recover old box
+			tft.drawRect(old_x, 0, BOX_X, BOX_Y, oldcolor);
+			// draw white box frame for the new color
+			if (p.x > X_BLACK) {
+				currentcolor = BLACK;
+				current_x = X_BLACK;
+			} else if (p.x > X_BLUE) {
+				currentcolor = BLUE;
+				current_x = X_BLUE;
+			} else if (p.x > X_RED) {
+				currentcolor = RED;
+				current_x = X_RED;
+			} else if (p.x > X_MAGENTA) {
+				currentcolor = MAGENTA;
+				current_x = X_MAGENTA;
+			} else if (p.x > X_GREEN) {
+				currentcolor = GREEN;
+				current_x = X_GREEN;
+			} else if (p.x > X_CYAN) {
+				currentcolor = CYAN;
+				current_x = X_CYAN;
+			} else if (p.x > X_YELLOW) {
+				currentcolor = YELLOW;
+				current_x = X_YELLOW;
+			} else if (p.x > X_WHITE) {
+				currentcolor = WHITE;
+				current_x = X_WHITE;
+			}
+			tft.drawRect(current_x, 0, BOX_X, BOX_Y, BLACK);
+			
+			oldcolor = currentcolor;
+			old_x = current_x;
 
-       if (oldcolor != currentcolor) {
-          if (oldcolor == RED) tft.fillRect(0, 0, BOXSIZE, BOXSIZE, RED);
-          if (oldcolor == YELLOW) tft.fillRect(BOXSIZE, 0, BOXSIZE, BOXSIZE, YELLOW);
-          if (oldcolor == GREEN) tft.fillRect(BOXSIZE*2, 0, BOXSIZE, BOXSIZE, GREEN);
-          if (oldcolor == CYAN) tft.fillRect(BOXSIZE*3, 0, BOXSIZE, BOXSIZE, CYAN);
-          if (oldcolor == BLUE) tft.fillRect(BOXSIZE*4, 0, BOXSIZE, BOXSIZE, BLUE);
-          if (oldcolor == MAGENTA) tft.fillRect(BOXSIZE*5, 0, BOXSIZE, BOXSIZE, MAGENTA);
-       }
-    }
-    if (((p.y-PENRADIUS) > BOXSIZE) && ((p.y+PENRADIUS) < tft.height())) {
-      tft.fillCircle(p.x, p.y, PENRADIUS, currentcolor);
-    }
-  }
+		}
+		// check point position, repetition and double click
+		if (((p.y-penradius) > (BOX_Y+PAINT_MARGIN)) && ((p.y+penradius) < tft.height())) {
+			// painting area
+			if (p.dbl) penradius = 3;
+			else if (penradius<p.repeat) penradius = p.repeat;
+			tft.fillCircle(p.x, p.y, penradius, currentcolor);
+		} else if ( p.y<BOX_Y ) {
+			if (p.dbl) {
+				// fill the writing area with current color
+				tft.fillRect(0, BOX_Y+PAINT_MARGIN, TFTWIDTH, TFTHEIGHT-BOX_Y-PAINT_MARGIN, currentcolor);
+			}
+		}
+		digitalWrite(LED_PIN, LOW);
+	}
 }
 
